e1849ccc576bece19a7fa9b7758fb0033934b5ee
jofetta <jafarovatatiana@gmail.com>
2024-04-14 20:04:25 +0300

    feat: solve two tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const pr = new Promise((resolve, reject) => {
+    if (number < 0) {
+      reject();
+    } else {
+      resolve();
+    }
+  });
+  return pr;
 }
 
 /**
@@ -33,8 +40,10 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source
+    .then(() => Promise.resolve('success'))
+    .catch(() => Promise.resolve('fail'));
 }
 
 /**
